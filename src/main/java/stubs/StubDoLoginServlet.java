9fc477bd4fbe1e0e79b71b7c39a2e3a203dc4b77
Ksenomorfa <black_estrella@mail.ru>
2017-08-03 20:12:20 +0300

    * small fixes in login/reg order
    Signed-off-by: Ksenomorfa <black_estrella@mail.ru>

--- src/main/java/stubs/StubDoLoginServlet.java
+++ src/main/java/stubs/StubDoLoginServlet.java
@@ -21,64 +21,63 @@ public class StubDoLoginServlet extends HttpServlet {
         HttpSession httpSession = request.getSession();
         String email = request.getParameter("email");
         String nonHashedPasswordReq = request.getParameter("password");
-        String passwordHashReq = "";
-        if (nonHashedPasswordReq != null && !email.isEmpty()) {
-            passwordHashReq = DigestUtils.md5Hex(nonHashedPasswordReq);
-        } else {
-            request.setAttribute("fieldEmpty",err.getString("fieldEmpty"));
+
+        if ((nonHashedPasswordReq == null || (email == null ||
+                nonHashedPasswordReq.isEmpty()) || email.isEmpty())) {
+            request.setAttribute("fieldEmpty", err.getString("fieldEmpty"));
             request.getRequestDispatcher("/WEB-INF/pages/login.jsp").forward(request, response);
-        }
 
-        String passwordHashDB = "";
-        User user = null;
-
-        UserService userService = new UserService();
-        Optional<User> userOptional = userService.get(email);
-        if (userOptional.isPresent()) {
-            user = userOptional.get();
-            passwordHashDB = user.getPasswordHash();
         } else {
-            request.setAttribute("nonexistentLogin", err.getString("nonexistentLogin"));
-            request.setAttribute("email", email);
-            request.getRequestDispatcher("/WEB-INF/pages/login.jsp").forward(request, response);
-        }
-
-        if (passwordHashDB.equals(passwordHashReq)) {
-            httpSession.setAttribute("user", user);
-
-            Cookie cookieUserId;
-            cookieUserId = new Cookie("userId", String.valueOf(user.getUserId()));
-            cookieUserId.setMaxAge(60 * 60 * 24 * 7); //one week
-            response.addCookie(cookieUserId);
-
-            List<Airport> airports = StubUtils.getAirports();
-            request.setAttribute("departures", airports);
-            request.setAttribute("arrivals", airports);
-
-            String dateFromString = (String) httpSession.getAttribute("dateFrom");
-            String dateToString = (String) httpSession.getAttribute("dateTo");
-            String departure = (String) httpSession.getAttribute("departureF");
-            String arrival = (String) httpSession.getAttribute("arrivalF");
-            String numberTicketsFilterString = (String) httpSession.getAttribute("numberTicketsFilter");
-            String businessString = (String) httpSession.getAttribute("businessString");
-            if ((dateFromString ==null) ||
-                    (dateToString == null) ||
-                    (departure==null)||
-                    (arrival==null) ||
-                    (numberTicketsFilterString == null)) {
-                response.sendRedirect("/");
+            String passwordHashDB = "";
+            User user = null;
+            String passwordHashReq = DigestUtils.md5Hex(nonHashedPasswordReq);
+            UserService userService = new UserService();
+            Optional<User> userOptional = userService.get(email);
+            if (userOptional.isPresent()) {
+                user = userOptional.get();
+                passwordHashDB = user.getPasswordHash();
+            } else {
+                request.setAttribute("nonexistentLogin", err.getString("nonexistentLogin"));
+                request.setAttribute("email", email);
+                request.getRequestDispatcher("/WEB-INF/pages/login.jsp").forward(request, response);
             }
-               else {
-                String redirectBackString = "/doSearch?dateFrom=" + dateFromString + "&dateTo=" + dateToString +
-                        "&selectedDeparture=" + departure + "&selectedArrival=" + arrival +
-                        "&numberTicketsFilter=" + numberTicketsFilterString + "&business="+businessString;
 
-                response.sendRedirect(redirectBackString);
+            if (passwordHashDB.equals(passwordHashReq)) {
+                httpSession.setAttribute("user", user);
+
+                Cookie cookieUserId;
+                cookieUserId = new Cookie("userId", String.valueOf(user.getUserId()));
+                cookieUserId.setMaxAge(60 * 60 * 24 * 7); //one week
+                response.addCookie(cookieUserId);
+
+                List<Airport> airports = StubUtils.getAirports();
+                request.setAttribute("departures", airports);
+                request.setAttribute("arrivals", airports);
+
+                String dateFromString = (String) httpSession.getAttribute("dateFrom");
+                String dateToString = (String) httpSession.getAttribute("dateTo");
+                String departure = (String) httpSession.getAttribute("departureF");
+                String arrival = (String) httpSession.getAttribute("arrivalF");
+                String numberTicketsFilterString = (String) httpSession.getAttribute("numberTicketsFilter");
+                String businessString = (String) httpSession.getAttribute("businessString");
+                if ((dateFromString == null) ||
+                        (dateToString == null) ||
+                        (departure == null) ||
+                        (arrival == null) ||
+                        (numberTicketsFilterString == null)) {
+                    response.sendRedirect("/");
+                } else {
+                    String redirectBackString = "/doSearch?dateFrom=" + dateFromString + "&dateTo=" + dateToString +
+                            "&selectedDeparture=" + departure + "&selectedArrival=" + arrival +
+                            "&numberTicketsFilter=" + numberTicketsFilterString + "&business=" + businessString;
+
+                    response.sendRedirect(redirectBackString);
+                }
+            } else {
+                request.setAttribute("loginFailed", err.getString("loginFailed"));
+                request.setAttribute("email", email);
+                request.getRequestDispatcher("/WEB-INF/pages/login.jsp").forward(request, response);
             }
-        } else {
-            request.setAttribute("loginFailed", err.getString("loginFailed"));
-            request.setAttribute("email", email);
-            request.getRequestDispatcher("/WEB-INF/pages/login.jsp").forward(request, response);
         }
     }
 
--- src/main/java/stubs/StubDoRegServlet.java
+++ src/main/java/stubs/StubDoRegServlet.java
@@ -24,31 +24,44 @@ public class StubDoRegServlet extends HttpServlet {
         String username = request.getParameter("username");
         String nonHashedPasswordFirstReq = request.getParameter("password1");
         String nonHashedPasswordSecondReq = request.getParameter("password2");
-        String password1HashReq = DigestUtils.md5Hex(nonHashedPasswordFirstReq);
-        String password2HashReq = DigestUtils.md5Hex(nonHashedPasswordSecondReq);
+        String password1HashReq = "";
+        String password2HashReq = "";
         LocalDateTime registrationDate = LocalDateTime.now();
 
-        if (nonHashedPasswordFirstReq.isEmpty() || nonHashedPasswordSecondReq.isEmpty() || username.isEmpty()) {
+        if (nonHashedPasswordFirstReq==null ||
+                nonHashedPasswordSecondReq==null ||
+                username==null ||
+                email ==null ||
+                nonHashedPasswordFirstReq.isEmpty() ||
+                nonHashedPasswordSecondReq.isEmpty() ||
+                username.isEmpty() ||
+               email.isEmpty()) {
             request.setAttribute("fieldEmpty", err.getString("fieldEmpty"));
             request.setAttribute("email", email);
             request.getRequestDispatcher("/WEB-INF/pages/registration.jsp").forward(request, response);
-        } else if (!password1HashReq.equals(password2HashReq)) {
-            request.setAttribute("passMismatch", err.getString("passMismatch"));
-            request.setAttribute("email", email);
-            request.setAttribute("username", username);
-            request.getRequestDispatcher("/WEB-INF/pages/registration.jsp").forward(request, response);
-        } else {
-            UserService userService = new UserService();
-            Optional<User> userOptional = userService.get(email);
-            if (userOptional.isPresent()) {
-                request.setAttribute("userAlreadyExists", err.getString("userAlreadyExists"));
+        }
+        else {
+            password1HashReq = DigestUtils.md5Hex(nonHashedPasswordFirstReq);
+            password2HashReq = DigestUtils.md5Hex(nonHashedPasswordSecondReq);
+
+            if (!password1HashReq.equals(password2HashReq)) {
+                request.setAttribute("passMismatch", err.getString("passMismatch"));
+                request.setAttribute("email", email);
+                request.setAttribute("username", username);
                 request.getRequestDispatcher("/WEB-INF/pages/registration.jsp").forward(request, response);
             } else {
-                User user = new User(username, email, password1HashReq, registrationDate);
-                userService.create(user);
-                request.setAttribute("regSuccess", err.getString("regSuccess"));
-                request.setAttribute("email", email);
-                request.getRequestDispatcher("/WEB-INF/pages/login.jsp").forward(request, response);
+                UserService userService = new UserService();
+                Optional<User> userOptional = userService.get(email);
+                if (userOptional.isPresent()) {
+                    request.setAttribute("userAlreadyExists", err.getString("userAlreadyExists"));
+                    request.getRequestDispatcher("/WEB-INF/pages/registration.jsp").forward(request, response);
+                } else {
+                    User user = new User(username, email, password1HashReq, registrationDate);
+                    userService.create(user);
+                    request.setAttribute("regSuccess", err.getString("regSuccess"));
+                    request.setAttribute("email", email);
+                    request.getRequestDispatcher("/WEB-INF/pages/login.jsp").forward(request, response);
+                }
             }
         }
     }

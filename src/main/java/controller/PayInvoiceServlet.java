b67c06b6cdeed6500bca7c994ccba158123a36fc
AlexSam <aronornone@gmail.com>
2017-08-08 22:34:42 +0300

    return cookie check

--- src/main/java/controller/PayInvoiceServlet.java
+++ src/main/java/controller/PayInvoiceServlet.java
@@ -21,6 +21,8 @@ public class PayInvoiceServlet extends HttpServlet {
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         ResourceBundle err = (ResourceBundle) getServletContext().getAttribute("errors");
         HttpSession httpSession = request.getSession();
+        Cookie[] cookies = request.getCookies();
+        SessionUtils.checkCookie(cookies, request, httpSession);
         User user = (User) httpSession.getAttribute("user");
         httpSession.setAttribute("lastServletPath", request.getServletPath());
         request.setCharacterEncoding("UTF-8");

a0c08e64dabc1901800768f7c563c15cc7638b04
sradichenko <rstason@gmail.com>
2017-08-09 13:49:34 +0300

    * code has been refactored

--- src/main/java/db/dao/DataSource.java
+++ src/main/java/db/dao/DataSource.java
@@ -1,6 +1,8 @@
 package db.dao;
 
 import org.apache.commons.dbcp2.BasicDataSource;
+import org.apache.log4j.Logger;
+import utils.ServletLog;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -10,63 +12,72 @@ import java.sql.SQLException;
 import java.util.Properties;
 
 public class DataSource {
-    private static String DRIVER_CLASS_NAME;
-    private static String DB_URL;
-    private static String DB_USER;
-    private static String DB_PASSWORD;
-    private static int CONN_POOL_SIZE;
-    private static int MIN_IDLE_CONNECTIONS;
-    private static int MAX_IDLE_CONNECTIONS;
-    private static int MAX_OPENED_PREP_STMTS;
+    private static Logger log = ServletLog.getLgDB();
+    private static String driverClassName;
+    private static String dbUrl;
+    private static String dbUser;
+    private static String dbPassword;
+    private static int connPoolSize;
+    private static int minIdleConnections;
+    private static int maxIdleConnections;
+    private static int maxOpenedPrepStmts;
 
     private static BasicDataSource dataSource;
     private static Connection connection;
 
     static {
+        log.info("static block: Initiazling empty properties.");
         Properties properties = new Properties();
 
+        log.info("static block: Trying to read properties from 'db.properties'.");
         try (BufferedReader br = new BufferedReader(
                 new InputStreamReader(
                     Thread.currentThread().getContextClassLoader()
                     .getResourceAsStream("db.properties")))) {
             properties.load(br);
         } catch (IOException e) {
-            e.printStackTrace();
+            log.error("static block: " + e);
         }
 
-        DRIVER_CLASS_NAME = properties.getProperty("driver");
-        DB_URL = properties.getProperty("url");
-        DB_USER = properties.getProperty("user");
-        DB_PASSWORD = properties.getProperty("password");
-        CONN_POOL_SIZE = Integer.parseInt(properties.getProperty("connection_pool_size"));
-        MIN_IDLE_CONNECTIONS = Integer.parseInt(properties.getProperty("min_idle_connections"));
-        MAX_IDLE_CONNECTIONS = Integer.parseInt(properties.getProperty("max_idle_connections"));
-        MAX_OPENED_PREP_STMTS = Integer.parseInt(properties.getProperty("max_opened_prepared_stmts"));
+        log.info("static block: Initiazling DB related field parameters by those received from 'db.propertiesfs'.");
+        driverClassName = properties.getProperty("driver");
+        dbUrl = properties.getProperty("url");
+        dbUser = properties.getProperty("user");
+        dbPassword = properties.getProperty("password");
+        connPoolSize = Integer.parseInt(properties.getProperty("connection_pool_size"));
+        minIdleConnections = Integer.parseInt(properties.getProperty("min_idle_connections"));
+        maxIdleConnections = Integer.parseInt(properties.getProperty("max_idle_connections"));
+        maxOpenedPrepStmts = Integer.parseInt(properties.getProperty("max_opened_prepared_stmts"));
     }
 
     private static void getDataSource() {
+        log.info("getDataSource(): Initializing dataSource field (of BasicDataSource apache class).");
         BasicDataSource bds = new BasicDataSource();
-        bds.setDriverClassName(DRIVER_CLASS_NAME);
-        bds.setUrl(DB_URL);
-        bds.setUsername(DB_USER);
-        bds.setPassword(DB_PASSWORD);
-        bds.setInitialSize(CONN_POOL_SIZE);
-        bds.setMinIdle(MIN_IDLE_CONNECTIONS);
-        bds.setMaxIdle(MAX_IDLE_CONNECTIONS);
-        bds.setMaxOpenPreparedStatements(MAX_OPENED_PREP_STMTS);
+        bds.setDriverClassName(driverClassName);
+        bds.setUrl(dbUrl);
+        bds.setUsername(dbUser);
+        bds.setPassword(dbPassword);
+        bds.setInitialSize(connPoolSize);
+        bds.setMinIdle(minIdleConnections);
+        bds.setMaxIdle(maxIdleConnections);
+        bds.setMaxOpenPreparedStatements(maxOpenedPrepStmts);
         dataSource = bds;
     }
 
     public static Connection getConnection() {
+        log.info("getConnection(): Trying to initialize connection.");
         try {
             if (connection == null || connection.isClosed()) {
-                if (dataSource == null) DataSource.getDataSource();
+                if (dataSource == null) {
+                    DataSource.getDataSource();
+                }
                 connection = dataSource.getConnection();
             }
         } catch(SQLException e) {
-            e.printStackTrace();
+            log.error("getConnection(): SQLException code: " + e.getErrorCode());
         }
 
+        log.info("getConnection(): Returning connection variable.");
         return connection;
     }
 }
